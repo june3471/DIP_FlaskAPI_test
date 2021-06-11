d1714321529606ba673c6df3f349212486d1aca5
aaronlee <leej3471@gmail.com>
2021-06-11 03:47:59 +0000

    add

---
+++ flask/app/app_main.py
@@ -0,0 +1,22 @@
+# About Flask and Flask_restful Library
+from flask import Flask, render_template,request
+from flask_restful import Api
+from model_FDS_serving import AiRun
+
+app = Flask(__name__)
+api = Api(app)
+
+#api.add_resource(<python 객체>,<api path : string>) 
+
+#REST api는 url 뒤에 /api/predict 과 같이 명시함
+#REpresentational State Transfer API
+#자원을 이름(자원의 표현)으로 구분하여 해당 자원의 상태(정보)를 주고 받는 모든 것을 의미
+api.add_resource(AiRun,"/api/predict")
+
+
+@app.route("/")
+def hello():
+    return render_template('/test.html')
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0',debug=True)
---
+++ flask/app/model_FDS_serving.py
@@ -0,0 +1,20 @@
+from flask_restful import Resource, reqparse, request
+import json
+
+class AiRun(Resource):
+    def predict_request(self, data):
+        ##Tensorflow Serving을 통해 예측 하는 로직 (request data 전처리 포함)
+        return data
+    def post(self):
+        ##요청값을 받아오고 결과값을 주기 위한 로직
+
+        #request를 파싱 하는 부분
+        parser = reqparse.RequestParser()
+        #type을 key로 한 값에 대해서 string 명시
+        parser.add_argument('type',type=str)
+        #data를 key로 한 값에 대해서 list형태 명시
+        parser.add_argument('data',action='append')
+        #파싱하여 args에 할당
+        args = parser.parse_args()
+
+        return args
