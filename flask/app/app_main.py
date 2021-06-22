cdc2ea018921c5ced7f862d66ae3a4d61c3240ab
aaronlee <leej3471@gmail.com>
2021-06-22 01:07:14 +0000

    add

--- flask/app/app_main.py
+++ flask/app/app_main.py
@@ -10,9 +10,9 @@ api = Api(app)
 
 #REST api는 url 뒤에 /api/predict 과 같이 명시함
 #REpresentational State Transfer API
+
 #자원을 이름(자원의 표현)으로 구분하여 해당 자원의 상태(정보)를 주고 받는 모든 것을 의미
 api.add_resource(AiRun,"/api/predict")
-
 
 @app.route("/")
 def hello():
